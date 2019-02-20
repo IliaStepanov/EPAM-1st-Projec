6cf2daab2f7355d81d6b5f36cc8c2e1f129edd5f
Ilia_Stepanov <Ilia_Stepanov@epam.com>
2019-02-20 10:40:33 +0300

    Fixing update personal data form with empty fields exception.

--- src/main/java/com/epam/lowcost/controller/ExceptionController.java
+++ src/main/java/com/epam/lowcost/controller/ExceptionController.java
@@ -4,8 +4,6 @@ import org.springframework.web.HttpSessionRequiredException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import static com.epam.lowcost.util.EndPoints.ENTRY;
-
 @ControllerAdvice
 public class ExceptionController {
     @ExceptionHandler(HttpSessionRequiredException.class)
