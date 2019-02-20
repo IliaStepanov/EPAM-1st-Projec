6fa605ca1ed1b3e1acae298ee3d5d3a440523306
TheDoctorZee <a_khamzin@mail.ru>
2019-02-20 12:12:48 +0300

    second_test

--- src/test/java/com/epam/lowcost/service/implementations/PlaneServiceImplTest.java
+++ src/test/java/com/epam/lowcost/service/implementations/PlaneServiceImplTest.java
@@ -6,16 +6,24 @@ import com.epam.lowcost.model.Plane;
 import com.epam.lowcost.service.interfaces.PlaneService;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlaneServiceImplTest {
 
+    private PlaneService planeService = mock(PlaneService.class);
 
     @Test
     public void getAllPlanes() {
+        List<Plane> expectedList = new ArrayList<>();
+        expectedList.add(new Plane(1, "Airbus A380",25,500,false));
+        when(planeService.getAllPlanes()).thenReturn(expectedList);
+        List<Plane> actualList = planeService.getAllPlanes();
+        assertEquals(actualList, expectedList);
     }
 
     @Test
     public void getById() {
-        PlaneService planeService = mock(PlaneServiceImpl.class);
         Plane expectedPlane = new Plane(1, "Airbus A380",25,500,false);
         when(planeService.getById(1)).thenReturn(expectedPlane);
         Plane actualPlane = planeService.getById(1);
