947300a096b55fa6a3e68565013e18874f031d7a
AnastasiaBerlina <47319972+AnastasiaBerlina@users.noreply.github.com>
2019-02-21 14:21:58 +0300

    Update UserServiceImpl.java

--- src/main/java/com/epam/lowcost/service/implementations/UserServiceImpl.java
+++ src/main/java/com/epam/lowcost/service/implementations/UserServiceImpl.java
@@ -5,8 +5,6 @@ import com.epam.lowcost.DAO.interfaces.UserDAO;
 import com.epam.lowcost.model.User;
 import com.epam.lowcost.service.interfaces.UserService;
 import org.mindrot.jbcrypt.BCrypt;
-
-import java.awt.*;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
